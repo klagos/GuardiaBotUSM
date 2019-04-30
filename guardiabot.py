8ad6a0e65acefe9c342ea8e9e8a5e6b2d3f37afd
Kevin Lagos <kevin.lagos@sansano.usm.cl>
2019-04-30 17:09:39 -0400

    feat : API POST and access_token

--- guardiabot.py
+++ guardiabot.py
@@ -1,2 +1,10 @@
 import requests
 
+app_properties = open("application.properties")
+access_token = app_properties.readlines()[0].split("=")[1]
+
+while True:
+	message = input("Mensaje a postear: ")
+	data = {'message': message, 'access_token': access_token}
+	r = requests.post("https://graph.facebook.com/278468619769380/feed", data)
+	print(r.status_code, r.reason)
